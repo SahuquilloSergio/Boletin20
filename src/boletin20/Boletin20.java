ceafd4d857317a43edcba0954ff5992cb3e30ad9
serxa <sergiose2010@gmail.com>
2018-02-26 09:21:18 +0100

    Main finished and delete method added

--- src/boletin20/Boletin20.java
+++ src/boletin20/Boletin20.java
@@ -1,12 +1,44 @@
 package boletin20;
 
 import com.sergio.libreria.Metodos;
+import java.io.File;
+import java.io.FileNotFoundException;
+import javax.swing.JOptionPane;
 
 public class Boletin20 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Metodos M1 = new Metodos();
-        M1.visualizar();
-    }
+        File file = new File("C:\\Users\\serxa\\OneDrive\\Documentos\\NetBeansProjects\\Boletin20\\files\\library.txt");
+        int op;
     
+    do{
+    op=Integer.parseInt(JOptionPane.showInputDialog("***MENÚ***\n"
+                                                        + "1. Añadir\n"
+                                                        + "2. Visualizar\n"
+                                                        + "3. Consultar\n"
+                                                        + "4. Modificar Precio\n"
+                                                        + "5. Eliminar Sen unidades\n"
+                                                        + "6. Salir"));
+    
+    switch(op){
+        
+        case 1: M1.añadir();
+                break;
+        
+        case 2: M1.visualizar();
+                break;
+        case 3: M1.consultar();
+                break;
+        case 4: M1.ModificarPrecio();
+                break;
+        case 5: M1.eliminarSenUnidades();
+                break;
+        case 6: System.exit(0);
+                break;
+        
+    }
+}while(op<7);
+     
+    }
 }
--- src/com/sergio/libreria/Metodos.java
+++ src/com/sergio/libreria/Metodos.java
@@ -2,64 +2,116 @@ package com.sergio.libreria;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class Metodos {
+
     File file;
     Scanner sc;
-    PrintWriter pw;
-    ArrayList<Libros> libro = new ArrayList();
-    
-    public void engadir() throws FileNotFoundException{
-        file = new File("C:\\Users\\serxa\\OneDrive\\Documentos\\NetBeansProjects\\Boletin20\\files\\library.txt");
-        try{
-        pw = new PrintWriter(file);
-        String titulo = JOptionPane.showInputDialog("Titulo");
-        String autor = JOptionPane.showInputDialog("Autor");
-        float precio = Float.parseFloat(JOptionPane.showInputDialog("Precio"));
-        int unidades = Integer.parseInt(JOptionPane.showInputDialog("Unidades"));
-        
-       libro.add(new Libros(titulo, autor, precio, unidades)); 
-       pw.println(titulo+autor+precio+unidades);
-        }catch(FileNotFoundException ex) {
-            System.out.println("Archivo no encontrado");
-        }catch(IOException ex1) {
-            System.out.println("Error");
-        } finally{
-            pw.close();
-        }
-    }
-    
-    public void visualizar(){
-        String linea;
-        String[] lista = new String[4];
-        Libros li;
-        Scanner sc = null;
-        
-        try{
-            sc = new Scanner(new File("C:\\Users\\serxa\\OneDrive\\Documentos\\NetBeansProjects\\Boletin20\\files\\library.txt"));
-            
-            while(sc.hasNextLine()){
-                linea = sc.nextLine();
-                lista = new String[4];
+    FileWriter escribir;
+    String linea;
+
+    File fichero = new File("C:\\Users\\serxa\\OneDrive\\Documentos\\NetBeansProjects\\Boletin20\\files\\library.txt");
+    ArrayList<Libro> libros = new ArrayList();
+    String lista[] = new String[4];
+
+    public Metodos() {
+        try {
+            sc = new Scanner(fichero);
+            while (sc.hasNext()) {
+                linea = sc.next();
                 lista = linea.split(",");
-                li = new Libros(lista[0], lista[1], Integer.parseInt(lista[2]), Integer.parseInt(lista[3]));
-                System.out.println(li);
+//                Libro l=new Libro(lista[0], lista[1], Integer.parseInt(lista[2], Integer.parseInt([3]));
+//                libros.add(l);
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("Error en la lectura del fichero");
+        } finally {
+            sc.close();
         }
-        
-    }catch (FileNotFoundException ex){
-            System.out.println("erro1" + ex.getMessage());
-    }
-        sc.close();
     }
 
+    public void añadir() {
+        Libro l = new Libro(JOptionPane.showInputDialog("Inserte Titulo"),
+                JOptionPane.showInputDialog("Inserte Autor"),
+                Integer.parseInt(JOptionPane.showInputDialog("Inserte Precio")),
+                Integer.parseInt(JOptionPane.showInputDialog("Inserte Unidades")));
+        libros.add(l);
 
+        try {
+            escribir = new FileWriter(fichero);
+            for (Libro element : libros) {
+                escribir.write(element.getTitulo() + ", " + element.getAutor() + ",  " + element.getPrecio() + ", " + element.getUnidades());
+            }
+            escribir.close();
+        } catch (IOException ex) {
+            System.out.println("Error a l escriir en el fichero");
+        }
+    }
 
+    public void visualizar() {
+        String todos = "";
 
+        for (Libro element : libros) {
+            todos = todos + element.toString() + "\n";
+        }
+
+        JOptionPane.showMessageDialog(null, todos);
+        todos = null;
+    }
+    
+    
+    public void ModificarPrecio() {
+        boolean var = false;
+
+        String nombre = JOptionPane.showInputDialog("Inserte Titulo");
+        int precio = Integer.parseInt(JOptionPane.showInputDialog("Inserte Nuevo Precio"));
+
+        for (Libro element : libros) {
+            if (nombre.equals(element.getTitulo())) {
+                element.setPrecio(precio);
+                var = true;
+            }
+        }
+
+    }
+
+    public void consultar() {
+
+        boolean var = false;
+        int precio = 0;
+
+        String nombre = JOptionPane.showInputDialog("Inserte Titulo");
+
+        for (Libro element : libros) {
+            if (nombre.equals(element.getTitulo())) {
+                precio = (int) element.getPrecio();
+                var = true;
+            }
+        }
+
+        if (var == false) {
+            JOptionPane.showMessageDialog(null, "Libro no disponible");
+        } else {
+            JOptionPane.showMessageDialog(null, precio);
+        }
+    }
+    
+    public void eliminarSenUnidades(){        
+        Iterator<Libro> it = libros.iterator();
+        while(it.hasNext()){
+            if(it.next().getUnidades()==0){
+                it.remove();
+            }
+        }
+        JOptionPane.showMessageDialog(null,"Libros sen unidades eliminados");
+    }
 
 
 }
